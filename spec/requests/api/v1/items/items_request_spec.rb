736118416e1fe338e8b80614d69ea3b529a62552
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-12-12 15:28:09 -0700

    Fix spacing and add test for updating an item

--- spec/requests/api/v1/items/items_request_spec.rb
+++ spec/requests/api/v1/items/items_request_spec.rb
@@ -4,8 +4,7 @@ RSpec.describe "Api::V1::Items", type: :request do
   describe "Items" do
     it "can get items" do
       merchant = create(:merchant)
-      item = create_list(:item, 3, merchant_id: merchant.id)
-
+      create_list(:item, 3, merchant_id: merchant.id)
 
       get "/api/v1/items"
       items_json = JSON.parse(response.body, symbolize_names: true)
@@ -22,40 +21,62 @@ RSpec.describe "Api::V1::Items", type: :request do
     end
 
     it "can get an item by its id" do
-    item = create(:item)
+      item = create(:item)
 
-    get "/api/v1/items/#{item.id}"
+      get "/api/v1/items/#{item.id}"
 
-    item_json = JSON.parse(response.body, symbolize_names: true)
+      item_json = JSON.parse(response.body, symbolize_names: true)
 
-    expect(response).to be_successful
-    expect(item_json.class).to eq(Hash)
-    expect(item_json[:data][:id]).to eq("#{item.id}")
-    expect(item_json[:data][:type]).to eq('item')
-    expect(item_json[:data][:attributes][:name]).to eq(item.name)
-    expect(item_json[:data][:attributes][:description]).to eq(item.description)
-    expect(item_json[:data][:attributes][:unit_price]).to eq(item.unit_price)
-    expect(item_json[:data][:attributes][:merchant_id]).to eq(item.merchant_id)
+      expect(response).to be_successful
+      expect(item_json.class).to eq(Hash)
+      expect(item_json[:data][:id]).to eq("#{item.id}")
+      expect(item_json[:data][:type]).to eq('item')
+      expect(item_json[:data][:attributes][:name]).to eq(item.name)
+      expect(item_json[:data][:attributes][:description]).to eq(item.description)
+      expect(item_json[:data][:attributes][:unit_price]).to eq(item.unit_price)
+      expect(item_json[:data][:attributes][:merchant_id]).to eq(item.merchant_id)
     end
 
     it "can create a new item" do
-    merchant = create(:merchant)
-    item_params = { name: "Samsung S25+ ", description: "It's just another phone", unit_price: 3000.47, merchant_id: merchant.id }
+      merchant = create(:merchant)
+      item_params = { name: "Samsung S25+ ", description: "It's just another phone", unit_price: 3000.47, merchant_id: merchant.id }
 
-    post '/api/v1/items', params: item_params
+      post '/api/v1/items', params: item_params
 
-    item_json = JSON.parse(response.body, symbolize_names: true)
-    item = Item.last
+      item_json = JSON.parse(response.body, symbolize_names: true)
+      item = Item.last
 
-    expect(response).to be_successful
-    expect(item_json.class).to eq(Hash)
-    expect(item.name).to eq(item_params[:name])
-    expect(item_json[:data][:id]).to eq("#{item.id}")
-    expect(item_json[:data][:type]).to eq('item')
-    expect(item_json[:data][:attributes][:name]).to eq(item_params[:name])
-    expect(item_json[:data][:attributes][:description]).to eq(item_params[:description])
-    expect(item_json[:data][:attributes][:unit_price]).to eq(item_params[:unit_price])
-    expect(item_json[:data][:attributes][:merchant_id]).to eq(item.merchant_id)
+      expect(response).to be_successful
+      expect(item_json.class).to eq(Hash)
+      expect(item.name).to eq(item_params[:name])
+      expect(item_json[:data][:id]).to eq("#{item.id}")
+      expect(item_json[:data][:type]).to eq('item')
+      expect(item_json[:data][:attributes][:name]).to eq(item_params[:name])
+      expect(item_json[:data][:attributes][:description]).to eq(item_params[:description])
+      expect(item_json[:data][:attributes][:unit_price]).to eq(item_params[:unit_price])
+      expect(item_json[:data][:attributes][:merchant_id]).to eq(item.merchant_id)
+    end
+
+    it "can update an exisiting item" do
+      item = create(:item)
+      previous_name = item.name
+
+      item_params = { name: "Random Name" }
+
+      patch "/api/v1/items/#{item.id}", params: item_params
+      new_item = Item.find(item.id)
+      item_json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+      expect(item_json.class).to eq(Hash)
+      expect(new_item.name).to eq(item_params[:name])
+      expect(new_item.name).to_not eq(previous_name)
+      expect(item_json[:data][:id]).to eq("#{item.id}")
+      expect(item_json[:data][:type]).to eq('item')
+      expect(item_json[:data][:attributes][:name]).to eq(item_params[:name])
+      expect(item_json[:data][:attributes][:description]).to eq(item.description)
+      expect(item_json[:data][:attributes][:unit_price]).to eq(item.unit_price)
+      expect(item_json[:data][:attributes][:merchant_id]).to eq(item.merchant_id)
     end
   end
 end
