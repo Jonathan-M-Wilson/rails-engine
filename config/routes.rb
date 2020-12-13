e7a758fc2a861f1cd1f21e136628aa164608b6ae
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-12-13 14:02:04 -0700

    Within namespace items add get to merchants index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+
+
+      namespace :items do
+        get '/:id/merchant', to: 'merchants#index'
+      end
+
+
       resources :merchants, only: [:index, :show]
 
       resources :items, only: [:index, :show, :create, :update, :destroy]
