af4f80d24aa18c0e544e8f56dee9d032e572a3af
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-12-14 11:45:19 -0700

    Correct rubocop offenses

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,12 +6,12 @@ class Item < ApplicationRecord
   has_many :invoices, through: :invoice_items
   has_many :transactions, through: :invoices
 
-  scope :find_by_date, -> (param) {where("to_char(#{param.keys.first},'YYYY-MM-DD-HH-MI-SS') ILIKE ?", "%#{param.values.first}%")}
-  scope :find_by_attribute, -> (param) {where("items.#{param.keys.first}::text ILIKE ?", "%#{param.values.first}%")}
+  scope :find_by_date, ->(param) { where("to_char(#{param.keys.first},'YYYY-MM-DD-HH-MI-SS') ILIKE ?", "%#{param.values.first}%") }
+  scope :find_by_attribute, ->(param) { where("items.#{param.keys.first}::text ILIKE ?", "%#{param.values.first}%") }
 
   def self.search(param)
     attribute = param.keys.first
-    if attribute == 'created_at' || attribute == 'updated_at'
+    if %w[created_at updated_at].include?(attribute)
       find_by_date(param).first
     else
       find_by_attribute(param).first
