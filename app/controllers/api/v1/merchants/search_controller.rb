12075ee4d8bf77be8258d3f307131ee3eedfec69
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-12-15 15:52:02 -0700

    Add index method to merchants search controller

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::Merchants::SearchController < ApplicationController
+  def index
+    render json: MerchantSerializer.new(Merchant.find_all_merchants(permitted_params))
+  end
+
   def show
     render json: MerchantSerializer.new(Merchant.find_merchant(permitted_params))
   end
