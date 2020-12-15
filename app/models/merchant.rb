5d52b687d4c9cb80730b3aff69d95a5b7cbfacb2
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-12-15 14:16:42 -0700

    Update method name from search to find_merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -8,7 +8,7 @@ class Merchant < ApplicationRecord
   scope :find_by_date, ->(param) { where("to_char(#{param.keys.first},'YYYY-MM-DD-HH-MI-SS') ILIKE ?", "%#{param.values.first}%") }
   scope :find_by_attribute, ->(param) { where("merchants.#{param.keys.first}::text ILIKE ?", "%#{param.values.first}%") }
 
-  def self.search(param)
+  def self.find_merchant(param)
     attribute = param.keys.first
     if %w[created_at updated_at].include?(attribute)
       find_by_date(param).first
