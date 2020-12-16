12d49db78c5c319cef54748541389e1e964da09e
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-12-16 14:15:03 -0700

    Add method in merchants model to find merchants with most items sold.

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -28,12 +28,23 @@ class Merchant < ApplicationRecord
 
   def self.most_revenue(param)
     Merchant
-    .select('merchants.*, sum(quantity * unit_price) as revenue')
+    .select('merchants.*, SUM(quantity * unit_price) AS revenue')
     .joins(invoices: [:invoice_items, :transactions])
     .merge(Transaction.successful)
     .merge(Invoice.successful)
     .group(:id)
     .order(revenue: :desc)
+    .limit(param)
+  end
+
+  def self.most_items_sold(param)
+    Merchant
+    .select('merchants.*, SUM(quantity) AS items_sold')
+    .joins(invoices: [:invoice_items, :transactions])
+    .merge(Transaction.successful)
+    .merge(Invoice.successful)
+    .group(:id)
+    .order(items_sold: :desc)
     .limit(param)
   end
 end
