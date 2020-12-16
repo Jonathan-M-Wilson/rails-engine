089f903221fa62cecacbb03dbbfc22e48142784e
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-12-16 14:14:06 -0700

    Create model test for new method most_items_sold and housekeeping

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -11,37 +11,20 @@ RSpec.describe Merchant, type: :model do
   end
 
   describe 'Methods' do
-    it ".find_merchant()" do
-      merchant = create(:merchant, name: "unIque nAme")
-      merchant_2 = create(:merchant, id: 2)
+    before :each do
+      @merchant_1 = create(:merchant, name: "unIque nAme")
+      @merchant_2 = create(:merchant, name: "unIque nAme")
+      @merchant_3 = create(:merchant, name: "unIque nAme")
+      @merchant_4 = create(:merchant, id: 2)
 
-      param = {"name"=>"unIque nAme"}
-      param_2 = {"id"=>"2"}
+      item_1 = create(:item, merchant: @merchant_1)
+      item_2 = create(:item, merchant: @merchant_2)
+      item_3 = create(:item, merchant: @merchant_3)
 
-      expect(Merchant.find_merchant(param)).to eq(merchant)
-      expect(Merchant.find_merchant(param_2)).to eq(merchant_2)
-    end
-
-    it ".find_all_merchants()" do
-      create_list(:merchant, 3, name: "unIque nAme")
-      param = {"name"=>"unIque nAme"}
-      expect(Merchant.find_all_merchants(param).size).to eq(3)
-    end
-
-    it ".most_revenue()" do
-      merchant_1 = create(:merchant)
-      merchant_2 = create(:merchant)
-      merchant_3 = create(:merchant)
-      merchant_4 = create(:merchant)
-
-      item_1 = create(:item, merchant: merchant_1)
-      item_2 = create(:item, merchant: merchant_2)
-      item_3 = create(:item, merchant: merchant_3)
-
-      invoice_1 = create(:invoice, merchant: merchant_1, status: 'shipped')
-      invoice_2 = create(:invoice, merchant: merchant_2, status: 'shipped')
-      invoice_3 = create(:invoice, merchant: merchant_3, status: 'shipped')
-      invoice_4 = create(:invoice, merchant: merchant_1, status: 'packaged')
+      invoice_1 = create(:invoice, merchant: @merchant_1, status: 'shipped')
+      invoice_2 = create(:invoice, merchant: @merchant_2, status: 'shipped')
+      invoice_3 = create(:invoice, merchant: @merchant_3, status: 'shipped')
+      invoice_4 = create(:invoice, merchant: @merchant_1, status: 'packaged')
 
       invoice_item_1 = create(:invoice_item, invoice: invoice_1, item: item_1, unit_price: 12.99, quantity: 30)
       invoice_item_2 = create(:invoice_item, invoice: invoice_2, item: item_1, unit_price: 12.99, quantity: 54)
@@ -52,8 +35,27 @@ RSpec.describe Merchant, type: :model do
       transaction_2 = create(:transaction, invoice: invoice_2)
       transaction_3 = create(:transaction, invoice: invoice_3)
       transaction_4 = create(:transaction, invoice: invoice_4)
+    end
 
-      expect(Merchant.most_revenue(1)).to eq([merchant_2])
+    it ".find_merchant()" do
+      param = {"name"=>"unIque nAme"}
+      param_2 = {"id"=>"2"}
+
+      expect(Merchant.find_merchant(param)).to eq(@merchant_1)
+      expect(Merchant.find_merchant(param_2)).to eq(@merchant_4)
+    end
+
+    it ".find_all_merchants()" do
+      param = {"name"=>"unIque nAme"}
+      expect(Merchant.find_all_merchants(param).size).to eq(3)
+    end
+
+    it ".most_revenue()" do
+      expect(Merchant.most_revenue(1)).to eq([@merchant_2])
+    end
+
+    it ".most_items_sold()" do
+      expect(Merchant.most_items_sold(1)).to eq([@merchant_2])
     end
   end
 end
