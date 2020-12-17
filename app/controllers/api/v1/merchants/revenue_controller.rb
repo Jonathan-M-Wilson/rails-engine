3b4d1fb8c99b5ae01b90e8cd17f6235b8c59b263
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-12-17 14:56:59 -0700

    Add show method to controller

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -2,4 +2,9 @@ class Api::V1::Merchants::RevenueController < ApplicationController
   def index
     render json: MerchantSerializer.new(Merchant.most_revenue(params[:quantity].to_i))
   end
+
+  def show
+    merchant = Merchant.find(params[:id])
+    render json: RevenueSerializer.new(RevenueFacade.revenue(merchant.revenue))
+  end
 end
