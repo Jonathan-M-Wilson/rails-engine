bf9879b91c1877d21336e57f25e5605ca8cb8f16
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-12-17 10:32:50 -0700

    Renamed most revenue controller to revenue controller

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::Merchants::MostRevenueController < ApplicationController
+class Api::V1::Merchants::RevenueController < ApplicationController
   def index
     render json: MerchantSerializer.new(Merchant.most_revenue(params[:quantity].to_i))
   end
