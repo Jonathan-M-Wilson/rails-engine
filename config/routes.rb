b6dfd5d142b5d52987b9d43ba0e28e8fdb64aa9c
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-12-17 16:14:34 -0700

    Remove blank space

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-
-
       namespace :items do
       #item's merchant
         get '/:id/merchants', to: 'merchants#index'
